d9ca915098a93c57c0cf3b45946d4ae4608b5e5d
Karol <karol144y@gmail.com>
2025-01-06 19:16:21 +0100

    dodaje usuwanie graczy z listy

---
+++ Team.cs
@@ -0,0 +1,49 @@
+﻿namespace zadanie3;
+
+public class Team
+{
+    private List<Player> players;
+
+    public Team()
+    {
+        players = new List<Player>();
+        Console.WriteLine("Stworzono nową drużynę");
+    }
+
+    public void AddPlayer(string name, string position, int score)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Błąd podaj normalne imie");
+            return;
+        }
+        
+        var player = new Player(name, position, score);
+        players.Add(player);
+        Console.WriteLine($"Dadano gracza: {name} na pozycje {position}");
+    }
+
+    public void RemovePlayer(string name)
+    {
+        Player playerToRemove = null;
+        foreach (var player in players)
+        {
+            if (player.Name == name)
+            {
+                playerToRemove = player;
+                break;
+            }
+            
+        }
+
+        if (playerToRemove != null)
+        {
+            players.Remove(playerToRemove);
+            Console.WriteLine($"Usunięto gracza: {name}");
+        }
+        else
+        {
+            Console.WriteLine($"Nie ma zawodnika o imieniu: {name}");
+        }
+    }
+}
--- obj/Debug/net9.0/zadanie3.AssemblyInfo.cs
+++ obj/Debug/net9.0/zadanie3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zadanie3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb1fd4db02d077d073071810306c392e8d2c88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8973d2c68ec01a396cb85f8470ea72be74275833")]
 [assembly: System.Reflection.AssemblyProductAttribute("zadanie3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zadanie3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
